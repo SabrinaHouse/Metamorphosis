2661816fa8d9a4a76ff6af0652b6651c0039fd03
SabrinaHouse <sabrinabhouse@gmail.com>
2025-11-18 22:42:02 -0600

    Add project files.

---
+++ Metamorphosis/Main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+#include <Box2D.h>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "METAMORPHOSIS");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
